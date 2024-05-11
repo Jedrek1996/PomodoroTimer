66b3a38f3b109d24c892f98152403de5af42614d
Jed <kohjedrek@yahoo.com.sg>
2024-05-11 17:16:23 +0800

    Updated audio sounds for buttons

--- app/GlobalRedux/Features/timerSlice.ts
+++ app/GlobalRedux/Features/timerSlice.ts
@@ -27,22 +27,24 @@ export const timerSlice = createSlice({
     startTimer: (state) => {
       state.isRunning = true;
       toast.success(TIMER_CONSTANTS.START);
-      // new Audio(pomodoroSuccessAlert).play();
+      new Audio(pomodoroSuccessAlert).play();
     },
     pauseTimer: (state) => {
       state.isRunning = false;
       toast.error(TIMER_CONSTANTS.PAUSE);
-      // new Audio(pomodoroErrorAlert).play();
+      new Audio(pomodoroErrorAlert).play();
     },
     resetTimer: (state) => {
       toast.error(TIMER_CONSTANTS.RESET);
       state.timer = 0;
       state.isRunning = false;
       state.isBreak = false;
+      new Audio(pomodoroErrorAlert).play();
     },
     endTimer: (state) => {
       state.timer = 0;
       state.isRunning = false;
+      new Audio(pomodoroErrorAlert).play();
     },
     tick: (state) => {
       if (state.isRunning || state.isBreak) {
@@ -55,19 +57,23 @@ export const timerSlice = createSlice({
       state.timer = 0;
       state.isBreak = true;
       state.cycles += 1;
+      new Audio(pomodoroSuccessAlert).play();
     },
     pauseBreakTimer: (state) => {
       state.isBreak = false;
+      new Audio(pomodoroErrorAlert).play();
     },
     resumeBreakTimer: (state) => {
       state.isBreak = true;
       state.isRunning = true;
+      new Audio(pomodoroSuccessAlert).play();
     },
     endBreak: (state) => {
       toast.error(TIMER_CONSTANTS.BREAK_END);
       state.timer = 0;
       state.isBreak = false;
       state.isRunning = true;
+      new Audio(pomodoroErrorAlert).play();
     },
   },
 });
