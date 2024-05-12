6549a50255bdc13b636aa9a5925e3d8a83914eee
Jed <kohjedrek@yahoo.com.sg>
2024-05-12 12:58:44 +0800

    Updated constants strings

--- app/constants/constants.ts
+++ app/constants/constants.ts
@@ -1,10 +1,10 @@
 export const TIMER_CONSTANTS = {
   HEADER: "Pomodoro Timer",
-  START: "Timer has started!",
-  PAUSE: "Timer has been paused",
-  RESET: "Timer has been resetted!",
-  END: "Timer has ended!",
-  BREAK: "Break has started",
-  BREAK_END: "Break has ended",
   CYCLES: "Completed Cycles: ",
+  START: "Timer started!",
+  PAUSE: "Timer paused!",
+  RESET: "Timer resetted!",
+  END: "Timer ended!",
+  BREAK: "Break started!",
+  BREAK_END: "Break ended!",
 };
